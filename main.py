bdd17e56020814414c5a40089420d515dfe9abf1
geovaniito <geovani.ito@escola.pr.gov.br>
2026-08-19 08:43:17 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+nome = input("Digite seu nome: ").strip()
+
+if len(nome) <= 3:
+    print("Nome curto")
+'''    
+alunos = [ "Gustavo", "Jonatan", "Felipe", "Enzo", "Julia", "Ana"]
+print(len(alunos))
