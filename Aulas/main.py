b50c5573f6dd1e4582f5a65df49d2166ca2d82d4
geovaniito <geovani.ito@escola.pr.gov.br>
2026-08-28 09:09:08 -0300

    Delete Aulas directory

--- Aulas/main.py
+++
@@ -1,8 +0,0 @@
-'''
-nome = input("Digite seu nome: ").strip()
-
-if len(nome) <= 3:
-    print("Nome curto")
-'''    
-alunos = [ "Gustavo", "Jonatan", "Felipe", "Enzo", "Julia", "Ana"]
-print(len(alunos))
