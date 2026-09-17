d8f7e84efcb1bab7d74351cfa8bb871225163bbd
geovaniito <geovani.ito@escola.pr.gov.br>
2026-09-17 10:28:16 -0300

    Add files via upload

---
+++ Aula 22 FUNÇÕES 2/main.py
@@ -0,0 +1,8 @@
+def apresentar (nome, idade)
+    print("Olá", nome, "Sua iadade é", idade)
+def somar(n1, n2):
+    return n1 + n2
+
+apresentar("Adenilson", 78)
+apresentar("João Cabeça", 19)
+apresentar("Pereira Gay", 666)
