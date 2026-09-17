3549c882c86ad75744aa3301d33a053e6ea52cb0
geovaniito <geovani.ito@escola.pr.gov.br>
2026-09-17 10:27:57 -0300

    Add files via upload

---
+++ Aula 21 FUNÇÕES/main.py
@@ -0,0 +1,13 @@
+print()
+int()
+#Funções são blocos de código que podem ser reutilizados:
+def calcular_media():
+    print("Calculando a média...")
+#Nome Parâmetro
+def saudacao(nome):
+    print(f"Bem vindo! {nome}")
+    
+calcular_media()
+saudacao("Geovani")
+saudacao("Pereira Gay")
+saudacao("Bernardi pé de lancha")
